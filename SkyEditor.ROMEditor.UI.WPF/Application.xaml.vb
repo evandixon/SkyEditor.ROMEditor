211e5dfe248979611106a0328f353c84bdba887c
Evan Dixon <evanldixon@live.com>
2016-07-02 15:13:32 -0500

    Added stub UI plugin

---
+++ SkyEditor.ROMEditor.UI.WPF/Application.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class Application
+    Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+
+    End Sub
+
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+
+End Class
