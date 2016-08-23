31eea8785ac114bf3dd96e27bcf2d7ff802a8c5c
Evan Dixon <evanldixon@live.com>
2016-08-23 16:29:38 -0500

    Improved usage of MediaToolkit

--- SkyEditor.ROMEditor.Windows/MenuActions/PsmdSoundtrackAction.vb
+++ SkyEditor.ROMEditor.Windows/MenuActions/PsmdSoundtrackAction.vb
@@ -1,6 +1,9 @@
 ﻿Imports System.IO
 Imports System.Reflection
 Imports System.Text.RegularExpressions
+Imports MediaToolkit
+Imports MediaToolkit.Model
+Imports MediaToolkit.Options
 Imports SkyEditor.Core.IO
 Imports SkyEditor.Core.Projects
 Imports SkyEditor.Core.UI
@@ -148,14 +151,13 @@ Namespace MenuActions
                                                Await vgmstream.RunVGMStream(vgmPath, source, destinationWav)
 
                                                'Convert to mp3
-                                               'Start awaiting so we can queue other processes
-                                               Await Task.Run(Sub()
-                                                                  Using e As New MediaToolkit.Engine(True)
-                                                                      Dim wav As New MediaToolkit.Model.MediaFile(destinationWav)
-                                                                      Dim mp3 As New MediaToolkit.Model.MediaFile(destinationMp3)
-                                                                      e.Convert(wav, mp3, New MediaToolkit.Options.ConversionOptions With {.AudioSampleRate = MediaToolkit.Options.AudioSampleRate.Hz48000})
-                                                                  End Using
-                                                              End Sub)
+                                               Using e As New Engine(True)
+                                                   Dim wav As New MediaFile(destinationWav)
+                                                   Dim mp3 As New MediaFile(destinationMp3)
+                                                   Dim options = New ConversionOptions
+                                                   options.AudioSampleRate = AudioSampleRate.Hz48000
+                                                   e.Convert(wav, mp3, options)
+                                               End Using
 
                                                IO.File.Delete(destinationWav)
 
@@ -165,6 +167,10 @@ Namespace MenuActions
                                                    With t.Tag
                                                        .Album = My.Resources.Language.PSMDSoundTrackAlbum
                                                        .AlbumArtists = {My.Resources.Language.PSMDSoundTrackArtist}
+#Disable Warning
+                                                       'Disabling warning because this tag needs to be set to ensure compatibility, like with Windows Explorer and Windows Media Player.
+                                                       .Artists = {My.Resources.Language.PSMDSoundTrackArtist}
+#Enable Warning
                                                        .Year = 2015
                                                        Dim filenameParts = trackNames(filename).Split(" ".ToCharArray, 2)
                                                        If filenameParts.Count = 2 Then
