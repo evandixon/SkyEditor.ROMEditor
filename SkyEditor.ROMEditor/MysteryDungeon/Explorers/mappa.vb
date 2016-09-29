e7c015df0320c4d84f237a1c57c5fb20d514de52
Evan Dixon <evanldixon@live.com>
2016-09-28 19:04:55 -0500

    Added pointers from content header to mappa_s

--- SkyEditor.ROMEditor/MysteryDungeon/Explorers/mappa.vb
+++ SkyEditor.ROMEditor/MysteryDungeon/Explorers/mappa.vb
@@ -1,8 +1,19 @@
-﻿Imports SkyEditor.ROMEditor.MysteryDungeon
+﻿Imports SkyEditor.Core.IO
+Imports SkyEditor.ROMEditor.MysteryDungeon
 
 Namespace MysteryDungeon.Explorers
     Public Class mappa
         Inherits Sir0
+
+        Public Overrides Async Function OpenFile(Filename As String, Provider As IOProvider) As Task
+            Await MyBase.OpenFile(Filename, Provider)
+
+            Dim ptr1 As Integer = BitConverter.ToInt32(Header, 0) 'Block of pointers, ends at pointer 2
+            Dim ptr2 As Integer = BitConverter.ToInt32(Header, 4) 'Block of data
+            Dim ptr3 As Integer = BitConverter.ToInt32(Header, 8) 'Block of pointers
+            Dim ptr4 As Integer = BitConverter.ToInt32(Header, &HC) 'Block of pointers
+            Dim ptr5 As Integer = BitConverter.ToInt32(Header, &H10)
+        End Function
     End Class
 End Namespace
 
