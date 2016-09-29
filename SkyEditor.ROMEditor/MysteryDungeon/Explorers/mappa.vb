948557db75f2fe0985024d7f30f025cddc4ba738
Evan Dixon <evanldixon@live.com>
2016-09-28 20:40:11 -0500

    Added notes to mappa_s

--- SkyEditor.ROMEditor/MysteryDungeon/Explorers/mappa.vb
+++ SkyEditor.ROMEditor/MysteryDungeon/Explorers/mappa.vb
@@ -8,11 +8,17 @@ Namespace MysteryDungeon.Explorers
         Public Overrides Async Function OpenFile(Filename As String, Provider As IOProvider) As Task
             Await MyBase.OpenFile(Filename, Provider)
 
-            Dim ptr1 As Integer = BitConverter.ToInt32(Header, 0) 'Block of pointers, ends at pointer 2
-            Dim ptr2 As Integer = BitConverter.ToInt32(Header, 4) 'Block of data
+            Dim ptr1 As Integer = BitConverter.ToInt32(Header, 0) 'Block of pointers, ends at pointer 2, appears to point to Floor Index
+            Dim ptr2 As Integer = BitConverter.ToInt32(Header, 4) 'Block of data, Floor Attribute Data, 32 byte entries
             Dim ptr3 As Integer = BitConverter.ToInt32(Header, 8) 'Block of pointers
+
+            'Pokemon Spawns
             Dim ptr4 As Integer = BitConverter.ToInt32(Header, &HC) 'Block of pointers
+            'Each pointer points to a dungeon's Pokemon entries, 8 byte entries, last entry in each group is empty
+
+            'Item Spawns
             Dim ptr5 As Integer = BitConverter.ToInt32(Header, &H10)
+            'Each pointer points to a 50 byte (0x32) entry
         End Function
     End Class
 End Namespace
