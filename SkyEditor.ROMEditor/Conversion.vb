c0b000848ff7fe1657e447757549d67c7bd8656a
Evan Dixon <evanldixon@live.com>
2016-10-17 18:38:49 -0500

    Fixed bad output from Conversion.Hex

--- SkyEditor.ROMEditor/Conversion.vb
+++ SkyEditor.ROMEditor/Conversion.vb
@@ -1,11 +1,11 @@
 ﻿Public Module Conversion
     Public Function Hex(value As Integer) As String
-        Return String.Join("", BitConverter.ToString(BitConverter.GetBytes(value)).Split("-").Reverse)
+        Return value.ToString("X")
     End Function
     Public Function Hex(value As UInteger) As String
-        Return String.Join("", BitConverter.ToString(BitConverter.GetBytes(value)).Split("-").Reverse)
+        Return value.ToString("X")
     End Function
     Public Function Hex(value As ULong) As String
-        Return String.Join("", BitConverter.ToString(BitConverter.GetBytes(value)).Split("-").Reverse)
+        Return value.ToString("X")
     End Function
 End Module
