de5207d3eede5b21925213c956cab78598683dc2
Evan Dixon <evanldixon@live.com>
2018-01-24 17:55:38 -0600

    Make POC code functional

--- Misc/Sandbox/Module1.vb
+++ Misc/Sandbox/Module1.vb
@@ -10,30 +10,60 @@ Module Module1
     Async Function MainAsync() As Task
         'Test pokemon_graphic.bin modification
         Dim provider = New PhysicalIOProvider
+
+        Dim pkmDb As New PGDB
+        Await pkmDb.OpenFile("C:\Users\evanl\Desktop\BaseRom\Raw Files\RomFS\pokemon_graphics_database.bin", provider)
+
         Dim pokemonGraphic As New Farc
         Await pokemonGraphic.OpenFile("C:\Users\evanl\Desktop\BaseRom\Raw Files\RomFS\pokemon_graphic.bin", provider)
 
-        Dim zoroaBgrs As New BGRS
-        Await zoroaBgrs.OpenFile("/zoroa_00.bgrs", pokemonGraphic)
+        For Each entry In pkmDb.Entries.Select(Function(x) x.SecondaryBgrsName & ".bgrs").Distinct().Concat(pkmDb.Entries.Select(Function(y) y.PrimaryBgrsFilename))
+            If Not String.IsNullOrEmpty(entry) Then
+                If pokemonGraphic.FileExists("/" & entry) Then
+                    Try
+                        Console.WriteLine("Processing " & entry)
+                        Dim zoroaBgrs As New BGRS
+                        Await zoroaBgrs.OpenFile("/" & entry, pokemonGraphic)
 
-        Dim sleepAnimation = zoroaBgrs.Animations.First(Function(a) a.AnimationName = "bd_sleep")
-        Dim sleepLoopAnimation = zoroaBgrs.Animations.First(Function(a) a.AnimationName = "bd_sleep")
+                        Dim sleepAnimation = zoroaBgrs.Animations.FirstOrDefault(Function(a) a.AnimationName = "bd_sleep")
+                        Dim sleepLoopAnimation = zoroaBgrs.Animations.FirstOrDefault(Function(a) a.AnimationName = "bd_sleeploop")
+                        Dim oldHrSleepAnimation = zoroaBgrs.Animations.FirstOrDefault(Function(a) a.AnimationName = "bd_ev001_sleep00")
+                        Dim oldHrSleepLoopAnimation = zoroaBgrs.Animations.FirstOrDefault(Function(a) a.AnimationName = "bd_ev001_sleep01")
 
-        Dim newSleepAnimation = sleepAnimation.Clone
-        Dim newSleepLoopAnimation = sleepLoopAnimation.Clone
+                        If sleepAnimation IsNot Nothing AndAlso sleepLoopAnimation IsNot Nothing AndAlso
+                                oldHrSleepAnimation Is Nothing AndAlso oldHrSleepLoopAnimation Is Nothing Then
 
-        newSleepAnimation.Name = sleepAnimation.Name.Replace("bd_sleep", "bd_ev001_sleep00")
-        newSleepAnimation.AnimationType = BGRS.AnimationType.RemoteSkeletalAnimation
-        newSleepLoopAnimation.Name = sleepLoopAnimation.Name.Replace("bd_sleeploop", "bd_ev001_sleep01")
-        newSleepLoopAnimation.AnimationType = BGRS.AnimationType.RemoteSkeletalAnimation
+                            Dim newSleepAnimation = sleepAnimation.Clone
+                            Dim newSleepLoopAnimation = sleepLoopAnimation.Clone
 
-        pokemonGraphic.CopyFile("/" & sleepAnimation.Name & ".bchskla", "/" & newSleepAnimation.Name & ".bchskla")
-        pokemonGraphic.CopyFile("/" & sleepLoopAnimation.Name & ".bchskla", "/" & newSleepLoopAnimation.Name & ".bchskla")
+                            newSleepAnimation.Name = sleepAnimation.Name.Replace("bd_sleep", "bd_ev001_sleep00")
+                            newSleepLoopAnimation.Name = sleepLoopAnimation.Name.Replace("bd_sleeploop", "bd_ev001_sleep01")
 
-        zoroaBgrs.Animations.Add(newSleepAnimation)
-        zoroaBgrs.Animations.Add(newSleepLoopAnimation)
+                            If newSleepAnimation.AnimationType And BGRS.AnimationType.SkeletalAnimation > 0 Then
+                                pokemonGraphic.CopyFile("/" & sleepAnimation.Name & ".bchskla", "/" & newSleepAnimation.Name & ".bchskla")
+                                pokemonGraphic.CopyFile("/" & sleepLoopAnimation.Name & ".bchskla", "/" & newSleepLoopAnimation.Name & ".bchskla")
+                            End If
 
-        Await zoroaBgrs.Save(pokemonGraphic)
+                            If newSleepAnimation.AnimationType And BGRS.AnimationType.MaterialAnimation > 0 Then
+                                pokemonGraphic.CopyFile("/" & sleepAnimation.Name & ".bchmata", "/" & newSleepAnimation.Name & ".bchmata")
+                                pokemonGraphic.CopyFile("/" & sleepLoopAnimation.Name & ".bchmata", "/" & newSleepLoopAnimation.Name & ".bchmata")
+                            End If
+
+                            zoroaBgrs.Animations.Add(newSleepAnimation)
+                            zoroaBgrs.Animations.Add(newSleepLoopAnimation)
+
+                            Await zoroaBgrs.Save(pokemonGraphic)
+                            Console.WriteLine("Did the thing")
+                        End If
+                    Catch ex As Exception
+                        Debugger.Break()
+                    End Try
+                End If
+            End If
+        Next
+
+
+
 
         ''Just for testing. Results should be obvious.
         'Dim zoroa2Bgrs As New BGRS
@@ -42,7 +72,7 @@ Module Module1
         'Await zoroa2Bgrs.Save(pokemonGraphic)
 
         Console.WriteLine("Saving farc")
-        Await pokemonGraphic.Save("C:\Users\evanl\Desktop\updated_pokemon_graphic.bin", provider)
+        Await pokemonGraphic.Save("C:\Users\evanl\Desktop\updated3_pokemon_graphic.bin", provider)
     End Function
 
 End Module
