cd5ea5bcabbc88e01a7cb0f289f6f7010578b231
Bork <31774924+SupaFresh@users.noreply.github.com>
2018-05-07 11:18:33 -0500

    Java exception handler in the works
    Working on making a exception handler that will catch exceptions caused when skyromeditor can't find Java installed on the user system

--- SkyEditor.ROMEditor.Windows/Windows/Java.vb
+++ SkyEditor.ROMEditor.Windows/Windows/Java.vb
@@ -1,15 +1,30 @@
-﻿Namespace Windows
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+Namespace Windows
     Public Class Java
 
         Public Shared Async Function RunJar(JarPath As String, Arguments As String, WorkingDirectory As String) As Task
             Dim args As New Text.StringBuilder
-            args.Append("-jar ")
-            args.Append($"""{JarPath}""")
+
+            Try
+                args.Append("-jar ")
+                args.Append($"""{JarPath}""")
+                Console.WriteLine("Try has Finished")
+            Catch ex As FileNotFoundException
+                MessageBox.Show("Java Not Found: " & ex.Message)
+            Catch ex As Exception
+                MessageBox.Show("Unknown Error occured when calling Java: " & ex.Message)
+            Finally
+                Console.WriteLine("Java exception handler finished")
+            End Try
+
             If Not String.IsNullOrEmpty(Arguments) Then
                 args.Append(" ")
                 args.Append(Arguments)
             End If
             Await ConsoleApp.RunProgram("java", args.ToString)
+
         End Function
     End Class
 End Namespace
