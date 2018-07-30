abe56faff5db4240b927dd012b8569a33fbae773
Evan Dixon <evanldixon@gmail.com>
2018-07-30 18:53:58 -0500

    Add failing test

---
+++ Tests/SkyEditor.ROMEditor.IntegrationTests/ProcessManagement/LuaTests.vb
@@ -0,0 +1,33 @@
+﻿Imports System.IO
+Imports SkyEditor.ROMEditor.ProcessManagement
+
+Namespace ProcessManagement
+
+    <TestClass>
+    Public Class LuaTests
+
+        <TestMethod>
+        Public Sub TestLuaDecompile()
+            Dim decompiled = My.Resources.compiled
+            Dim testFilename = "LuaTestCompiled.lua"
+            Dim outputFilename = "LuaTestDecomp.lua"
+            File.WriteAllBytes(testFilename, decompiled)
+
+            Try
+                Using unluacManager As New UnluacManager
+                    unluacManager.DecompileScript(testFilename, outputFilename).Wait()
+
+                    Assert.IsTrue(File.Exists(outputFilename), "Output file was not created.")
+
+                    Dim actual = File.ReadAllText(outputFilename)
+                    Assert.AreEqual(My.Resources.testScript, actual, "Decompiled script does not match source")
+                End Using
+            Finally
+                If File.Exists("LuaTestScript.lua") Then
+                    File.Delete("LuaTestScript.lua")
+                End If
+            End Try
+        End Sub
+    End Class
+
+End Namespace
---
+++ Tests/SkyEditor.ROMEditor.IntegrationTests/Resources/lua/testScript.lua
@@ -0,0 +1,4 @@
+function addTheNumbers(numberA, numberB)
+  return numberA + numberB
+end
+print(addTheNumbers(10, 20))
