e04b3e94b22cf858411b007652991e6a3d1be992
Evan Dixon <evanldixon@live.com>
2018-10-12 13:05:46 -0500

    Add test utility to hash FARC filenames

---
+++ Misc/FarcFilenameHasher/Program.cs
@@ -0,0 +1,18 @@
+﻿using SkyEditor.ROMEditor.MysteryDungeon.PSMD;
+using System;
+
+namespace FarcFilenameHasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter file name to hash: ");
+                var hash = PmdFunctions.Crc32Hash(Console.ReadLine());
+                Console.WriteLine(hash.ToString("X").PadLeft(8, '0'));
+            }
+        }
+    }
+}
