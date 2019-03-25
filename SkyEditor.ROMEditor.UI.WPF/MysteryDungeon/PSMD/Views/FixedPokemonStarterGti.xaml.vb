83d2237d80767615c7981e36584847684dec8b26
Dixon, Evan <evanldixon@gmail.com>
2019-03-25 13:43:38 -0500

    Fix using wrong control

--- SkyEditor.ROMEditor.UI.WPF/MysteryDungeon/PSMD/Views/FixedPokemonStarterGti.xaml.vb
+++ SkyEditor.ROMEditor.UI.WPF/MysteryDungeon/PSMD/Views/FixedPokemonStarterGti.xaml.vb
@@ -1,12 +1,16 @@
-﻿Imports SkyEditor.ROMEditor.UI.WPF.MysteryDungeon.PSMD.ViewModels
+﻿Imports SkyEditor.Core.UI
+Imports SkyEditor.ROMEditor.UI.WPF.MysteryDungeon.PSMD.ViewModels
 Imports SkyEditor.UI.WPF
 
 Namespace MysteryDungeon.PSMD.Views
     Public Class FixedPokemonStarterGti
         Inherits DataBoundViewControl
+        Implements IViewControl
 
-        Public Overrides Function SupportsObject(Obj As Object) As Boolean
-            Return MyBase.SupportsObject(Obj) AndAlso TypeOf Obj Is FixedPokemonStarterViewModel AndAlso TypeOf DirectCast(Obj, FixedPokemonStarterViewModel).StarterEntries.First() Is FixedPokemonEntryGtiViewModel
+        Public Overrides Function SupportsObject(Obj As Object) As Boolean Implements IViewControl.SupportsObject
+            Return MyBase.SupportsObject(Obj) AndAlso
+                TypeOf Obj Is FixedPokemonStarterViewModel AndAlso
+                TypeOf DirectCast(Obj, FixedPokemonStarterViewModel).StarterEntries.First() Is FixedPokemonEntryGtiViewModel
         End Function
     End Class
 
--- SkyEditor.ROMEditor.UI.WPF/MysteryDungeon/PSMD/Views/FixedPokemonStarterPsmd.xaml.vb
+++ SkyEditor.ROMEditor.UI.WPF/MysteryDungeon/PSMD/Views/FixedPokemonStarterPsmd.xaml.vb
@@ -1,13 +1,18 @@
-﻿Imports SkyEditor.ROMEditor.UI.WPF.MysteryDungeon.PSMD.ViewModels
+﻿Imports SkyEditor.Core.UI
+Imports SkyEditor.ROMEditor.UI.WPF.MysteryDungeon.PSMD.ViewModels
 Imports SkyEditor.UI.WPF
 
 Namespace MysteryDungeon.PSMD.Views
     Public Class FixedPokemonStarterPsmd
         Inherits DataBoundViewControl
+        Implements IViewControl
 
-        Public Overrides Function SupportsObject(Obj As Object) As Boolean
-            Return MyBase.SupportsObject(Obj) AndAlso TypeOf Obj Is FixedPokemonStarterViewModel AndAlso TypeOf DirectCast(Obj, FixedPokemonStarterViewModel).StarterEntries.First() Is FixedPokemonEntryPsmdViewModel
+        Public Overrides Function SupportsObject(Obj As Object) As Boolean Implements IViewControl.SupportsObject
+            Return MyBase.SupportsObject(Obj) AndAlso
+                TypeOf Obj Is FixedPokemonStarterViewModel AndAlso
+                TypeOf DirectCast(Obj, FixedPokemonStarterViewModel).StarterEntries.First() Is FixedPokemonEntryPsmdViewModel
         End Function
     End Class
+
 End Namespace
 
